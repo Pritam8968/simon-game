88472f327b67407b651a8b63ae4a8026a3a9abf3
Pritam Sarkar <pritamsarkar9883@gmail.com>
2022-11-02 15:12:44 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# The Simon Game
+
+visit the link: https://pritam8968.github.io/simon-game/
---
+++ game.js
@@ -0,0 +1,110 @@
+// all the buttons
+var buttonColors = ["red", "blue", "green", "yellow"];
+
+// game pattern and user clicked pattern so far
+var gamePattern = [];
+var userClickedPattern = [];
+
+var level = 0;
+
+//start the game
+$(".start-btn").click(function () {
+	hideButton(this);
+	nextSequence();
+	showLevel();
+});
+
+function hideButton(btn) {
+	$(btn).css("display", "none");
+}
+
+// The random color generator
+function nextSequence() {
+	// resetting the user clicked pattern
+	userClickedPattern = [];
+
+	let randomNumber = Math.floor(Math.random() * 4);
+	let randomChosenColor = buttonColors[randomNumber];
+
+	// Saving the pattern so far
+	gamePattern.push(randomChosenColor);
+
+	playSound(randomChosenColor);
+	$("#" + randomChosenColor)
+		.fadeIn(100)
+		.fadeOut(100)
+		.fadeIn(100);
+
+	level++;
+	showLevel();
+}
+
+function showLevel() {
+	$("#level-title").text("Level: " + level);
+}
+
+// user play area
+$(".btn").on("click", function () {
+	let userChosenColor = this.id;
+	userClickedPattern.push(userChosenColor);
+
+	playSound(userChosenColor);
+	animatePress(userChosenColor);
+
+	// console.log(userClickedPattern);
+	matchPattern(userClickedPattern.length - 1);
+});
+
+// sound-playing function
+function playSound(name) {
+	var sound = new Audio("sounds/" + name + ".mp3");
+	sound.play();
+}
+
+// pressing animation function
+function animatePress(key) {
+	$("#" + key).addClass("pressed");
+	setTimeout(() => {
+		$("#" + key).removeClass("pressed");
+	}, 100);
+}
+
+function matchPattern(currentLevel) {
+	// console.log(userClickedPattern);
+	// console.log(gamePattern);
+	// If the user got the most recent answer right
+	if (userClickedPattern[currentLevel] === gamePattern[currentLevel]) {
+		// if they have finished their sequence
+		if (userClickedPattern.length === gamePattern.length) {
+			console.log("success!");
+			setTimeout(() => {
+				nextSequence();
+			}, 1000);
+		} else {
+			console.log("go on");
+		}
+	} else {
+		console.log("wrong");
+		gameOver();
+	}
+}
+
+function gameOver() {
+	playSound("wrong");
+	$("body").addClass("game-over");
+	setTimeout(() => {
+		$("body").removeClass("game-over");
+	}, 100);
+	$("#level-title").text("game over!");
+	$(".start-btn").text("Restart");
+	showButton();
+	$(".start-btn").click(startOver());
+}
+
+function showButton() {
+	$(".start-btn").css("display", "inline-block");
+}
+function startOver() {
+	level = 0;
+	gamePattern = [];
+}
